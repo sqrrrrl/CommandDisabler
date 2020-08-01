0e59f97e193b8a16c7e1424454bd53e6025bf660
superbobby2000 <48035361+superbobby2000@users.noreply.github.com>
2020-08-01 17:36:43 -0400

    first

---
+++ src/superbobby2000/CommandDisabler/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace superbobby2000\CommandDisabler;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+
+    public function onEnable() {
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+        foreach ($this->getConfig()->get("Disabled-Commands") as $command) {
+            $commandMap = $this->getServer()->getCommandMap();
+            $cmd = $commandMap->getCommand($command);
+            if ($cmd == null) {
+                $this->getLogger()->error("Command /" . $command . " not found");
+                return false;
+            }
+            $commandMap->unregister($cmd);
+        }
+        return true;
+    }
+}
